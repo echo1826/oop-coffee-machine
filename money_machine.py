45310cb77e54861e0f6ba4e3a2c5aaf8ee927e2c
Ethan Cho <ethan.cho1826@gmail.com>
2022-07-15 15:53:25 -0500

    refactored code to fix bugs and work better with current logic

--- money_machine.py
+++ money_machine.py
@@ -15,18 +15,20 @@ class MoneyMachine:
     def report(self):
         print(f"Money: ")
 
-    def insert_coins(self, order):
+    def insert_coins(self):
         print("Please insert coins.")
         for coin in self.COIN_VALUE:
             self.money_received += int(input(f"How many {coin}?: ")) * self.COIN_VALUE[coin]
         return self.money_received
 
     def process_payment(self, price):
+        self.insert_coins()
         if self.money_received < price :
             print("Sorry that's not enough money. Money refunded...")
             self.money_received = 0
             return False
         change = round(self.money_received - price, 2)
+        print(f"Here is {self.CURRENCY}{change:.2f} in change.")
         self.profit += price
         return True
         
